f22dd12a91cd4e50a8c9011a1dc49fb02ad2a8b5
Erik Schauer <do1ffe@darc.de>
2023-11-29 17:42:33 +0100

    Update statistik.py

--- statistik.py
+++ statistik.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, send_file, render_template_string
 import pandas as pd
-import io
 import os
 from datetime import datetime
 from openpyxl.utils import get_column_letter
@@ -9,9 +8,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def upload_file_form():
+    # Aktuelles Jahr ermitteln für das Copyright
     current_year = datetime.now().year
     copyright_year = "2023" if current_year == 2023 else f"2023-{current_year}"
 
+    # HTML-Formular für Datei-Upload
     return '''
     <html>
        <body>
@@ -33,34 +34,38 @@ def upload_file():
         if not f:
             return 'Keine Datei hochgeladen', 400
 
+        # Einlesen der Excel-Datei in einen DataFrame
         df = pd.read_excel(f)
 
-        # Konvertieren der 'Datum'-Spalte zu datetime mit ISO 8601 Format
-        df['Datum'] = pd.to_datetime(df['Datum'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
-        df = df.dropna(subset=['Datum'])
-
+        # Extrahieren der ersten zwei Buchstaben der Tickettypen und Gruppierung
         df['Tickettyp'] = df['Quittung Text'].str[:2]
         ticket_types = ['TT', 'MT', 'JT', 'V', 'R']
         df = df[df['Tickettyp'].isin(ticket_types)]
+        grouped_data = df.groupby([df['Datum'], 'Tickettyp']).size().unstack(fill_value=0)
 
-        grouped_data = df.groupby([df['Datum'].dt.strftime('%d.%m.%Y'), 'Tickettyp']).size().unstack(fill_value=0)
+        # Pfad zur CSV-Datei
+        csv_file_path = 'Statistiken/statistik.csv'
 
-        for ticket_type in ['TT', 'MT', 'JT', 'V', 'R']:
-            if ticket_type not in grouped_data:
-                grouped_data[ticket_type] = 0
+        # Laden bestehender Daten aus der CSV-Datei, falls vorhanden
+        if os.path.exists(csv_file_path):
+            existing_data = pd.read_csv(csv_file_path)
+            existing_data['Datum'] = pd.to_datetime(existing_data['Datum'])
+        else:
+            existing_data = pd.DataFrame()
 
-        grouped_data = grouped_data[['TT', 'MT', 'JT', 'V', 'R']]
+        # Zusammenführen der neuen Daten mit den bestehenden Daten
+        combined_data = pd.concat([existing_data, grouped_data.reset_index()])
+        combined_data = combined_data.drop_duplicates(subset=['Datum'], keep='last')
+        combined_data.to_csv(csv_file_path, index=False)
 
-        # Korrektur: Hinzufügen der Gesamtsumme
-        grouped_data.loc['Gesamt'] = grouped_data.sum()
-
+        # Erstellen der Excel-Datei und Anpassen der Spaltenbreite
         current_date = datetime.now().strftime('%y%m%d')
         filename = f"{current_date}-Kassenbuch-Statistik.xlsx"
         os.makedirs('Statistiken', exist_ok=True)
         local_path = os.path.join('Statistiken', filename)
 
         with pd.ExcelWriter(local_path, engine='openpyxl') as writer:
-            grouped_data.to_excel(writer)
+            combined_data.to_excel(writer, index=False)
             workbook = writer.book
             worksheet = writer.sheets['Sheet1']
 
@@ -68,7 +73,8 @@ def upload_file():
                 max_length = max(len(str(cell.value)) for cell in col)
                 worksheet.column_dimensions[get_column_letter(col[0].column)].width = max_length + 2
 
-        html_table = grouped_data.to_html(classes='table table-striped')
+        # Erstellen der HTML-Tabelle mit allen Daten
+        html_table = combined_data.to_html(classes='table table-striped', index=False)
 
         return render_template_string(f'''
         <html>
