15564bd09b909be8737ad49b10d8c4810222047c
Erik Schauer <do1ffe@darc.de>
2023-11-29 18:06:32 +0100

    Update statistik.py

--- statistik.py
+++ statistik.py
@@ -8,11 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def upload_file_form():
-    # Aktuelles Jahr ermitteln für das Copyright
     current_year = datetime.now().year
     copyright_year = "2023" if current_year == 2023 else f"2023-{current_year}"
 
-    # HTML-Formular für Datei-Upload
     return '''
     <html>
        <body>
@@ -36,39 +34,33 @@ def upload_file():
 
         df = pd.read_excel(f)
 
-        # Extrahieren der ersten zwei Buchstaben der Tickettypen und Gruppierung
+        # Datumskonvertierung
+        df['Datum'] = pd.to_datetime(df['Datum']).dt.strftime('%d.%m.%Y')
+
         df['Tickettyp'] = df['Quittung Text'].str[:2]
         ticket_types = ['TT', 'MT', 'JT', 'V', 'R']
         df = df[df['Tickettyp'].isin(ticket_types)]
-        grouped_data = df.groupby([df['Datum'], 'Tickettyp']).size().unstack(fill_value=0)
+        grouped_data = df.groupby(['Datum', 'Tickettyp']).size().unstack(fill_value=0)
 
-        # Stellen Sie sicher, dass alle Tickettypen vorhanden sind
         for ticket_type in ticket_types:
             if ticket_type not in grouped_data:
                 grouped_data[ticket_type] = 0
 
-        # Anordnen der Spalten in der gewünschten Reihenfolge
-        desired_order = ['TT', 'MT', 'JT', 'V', 'R']
-        grouped_data = grouped_data[desired_order]
-
-        # Hinzufügen der Gesamtsummenzeile
+        grouped_data = grouped_data[ticket_types]
         grouped_data.loc['Gesamt'] = grouped_data.sum()
 
-        # Pfad zur CSV-Datei
         csv_file_path = 'Statistiken/statistik.csv'
 
-        # Laden bestehender Daten aus der CSV-Datei, falls vorhanden
         if os.path.exists(csv_file_path):
             existing_data = pd.read_csv(csv_file_path)
         else:
             existing_data = pd.DataFrame()
 
-        # Zusammenführen der neuen Daten mit den bestehenden Daten
         combined_data = pd.concat([existing_data, grouped_data.reset_index()])
         combined_data = combined_data.drop_duplicates(subset=['Datum'], keep='last')
-        combined_data.to_csv(csv_file_path, index=False)
 
-        # Erstellen der Excel-Datei und Anpassen der Spaltenbreite
+        combined_data.to_csv(csv_file_path, index=False, date_format='%d.%m.%Y')
+
         current_date = datetime.now().strftime('%y%m%d')
         filename = f"{current_date}-Kassenbuch-Statistik.xlsx"
         os.makedirs('Statistiken', exist_ok=True)
@@ -83,7 +75,6 @@ def upload_file():
                 max_length = max(len(str(cell.value)) for cell in col)
                 worksheet.column_dimensions[get_column_letter(col[0].column)].width = max_length + 2
 
-        # Erstellen der HTML-Tabelle mit allen Daten und Gesamtsummenzeile
         html_table = combined_data.to_html(classes='table table-striped', index=False)
 
         return render_template_string(f'''
