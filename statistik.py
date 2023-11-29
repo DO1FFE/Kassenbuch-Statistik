ed486b605fe63182a0da3f2360313f66b923d94f
Erik Schauer <do1ffe@darc.de>
2023-11-29 22:04:34 +0100

    Update statistik.py

--- statistik.py
+++ statistik.py
@@ -1,46 +1,55 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template_string, redirect, url_for, session
 import pandas as pd
 import os
 from datetime import datetime
-from openpyxl.utils import get_column_letter
+import glob
 
 app = Flask(__name__)
+app.secret_key = 'IhrSehrGeheimerSchlüssel'
 
-@app.route('/')
-def upload_file_form():
-    current_year = datetime.now().year
-    copyright_year = "2023" if current_year == 2023 else f"2023-{current_year}"
+VORGESEHENES_PASSWORT = 'IhrPasswort'
 
+@app.route('/', methods=['GET', 'POST'])
+def password_form():
+    if request.method == 'POST':
+        password = request.form.get('password')
+        if password == VORGESEHENES_PASSWORT:
+            session['authenticated'] = True
+            return redirect(url_for('upload_file_form'))
+        else:
+            return '<p>Falsches Passwort. Bitte versuchen Sie es erneut.</p>' + render_password_form()
+
+    return render_password_form()
+
+def render_password_form():
     return '''
     <html>
        <body>
-          <h1>Konvertierung Kassenbuch in Statistik</h1>
-          <p>Bitte laden Sie das aktuelle Kassenbuch im Excel-Format hoch.</p>
-          <form action="/upload" method="post" enctype="multipart/form-data">
-             <input type="file" name="file" />
+          <h1>Passwort eingeben</h1>
+          <form action="/" method="post">
+             Passwort: <input type="password" name="password" />
              <input type="submit" />
           </form>
-          <p>&copy; Copyright by Erik Schauer, ''' + copyright_year + ''', do1ffe@darc.de</p>
        </body>
     </html>
     '''
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file_form():
+    if 'authenticated' not in session:
+        return redirect(url_for('password_form'))
+
     if request.method == 'POST':
         f = request.files['file']
         if not f:
             return 'Keine Datei hochgeladen', 400
 
-        # Einlesen der Excel-Datei ab Zeile 4
         df = pd.read_excel(f, skiprows=3)
-
-        # Datumskonvertierung der ersten Spalte
         df.iloc[:, 0] = pd.to_datetime(df.iloc[:, 0]).dt.strftime('%d.%m.%Y')
-        df.rename(columns={df.columns[0]: 'Datum'}, inplace=True)
+        df.rename(columns={df.columns[0]: 'Datum', df.columns[7]: 'Tickettyp'}, inplace=True)
 
-        df['Tickettyp'] = df['Quittung Text'].str[:2]
         ticket_types = ['TT', 'MT', 'JT', 'V', 'R']
+        df['Tickettyp'] = df['Tickettyp'].str[:2]
         df = df[df['Tickettyp'].isin(ticket_types)]
         grouped_data = df.groupby(['Datum', 'Tickettyp']).size().unstack(fill_value=0)
 
@@ -52,7 +61,6 @@ def upload_file():
         grouped_data.loc['Gesamt'] = grouped_data.sum()
 
         csv_file_path = 'Statistiken/statistik.csv'
-
         if os.path.exists(csv_file_path):
             existing_data = pd.read_csv(csv_file_path)
         else:
@@ -60,7 +68,6 @@ def upload_file():
 
         combined_data = pd.concat([existing_data, grouped_data.reset_index()])
         combined_data = combined_data.drop_duplicates(subset=['Datum'], keep='last')
-
         combined_data.to_csv(csv_file_path, index=False, date_format='%d.%m.%Y')
 
         current_date = datetime.now().strftime('%y%m%d')
@@ -70,33 +77,25 @@ def upload_file():
 
         with pd.ExcelWriter(local_path, engine='openpyxl') as writer:
             combined_data.to_excel(writer, index=False)
-            workbook = writer.book
-            worksheet = writer.sheets['Sheet1']
 
-            for col in worksheet.columns:
-                max_length = max(len(str(cell.value)) for cell in col)
-                worksheet.column_dimensions[get_column_letter(col[0].column)].width = max_length + 2
+    # Pfad zur neuesten Statistik-Datei finden
+    list_of_files = glob.glob('Statistiken/*.xlsx') 
+    latest_file = max(list_of_files, key=os.path.getctime) if list_of_files else None
+    latest_file_name = os.path.basename(latest_file) if latest_file else "Keine Statistik verfügbar"
 
-        html_table = combined_data.to_html(classes='table table-striped', index=False)
-
-        return render_template_string(f'''
-        <html>
-            <head>
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-                <style>
-                    .table {{ width: auto !important; margin: auto; }}
-                </style>
-            </head>
-            <body>
-                <h2>Statistik-Tabelle</h2>
-                {html_table}
-                <br>
-                <a href="/download/{filename}">Excel-Datei herunterladen</a>
-                <br><br>
-                <a href="/">Zurück zur Hauptseite</a>
-            </body>
-        </html>
-        ''')
+    return render_template_string('''
+    <html>
+       <body>
+          <h1>Kassenbuch hochladen</h1>
+          <form action="/upload" method="post" enctype="multipart/form-data">
+             <input type="file" name="file" />
+             <input type="submit" />
+          </form>
+          <br>
+          <a href="/download/{{ latest_file_name }}">{{ latest_file_name }}</a>
+       </body>
+    </html>
+    ''', latest_file_name=latest_file_name)
 
 @app.route('/download/<filename>')
 def download_file(filename):
